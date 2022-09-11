1953ed59600cf40dd81110375e6f744cdc5d8ea9
Eugen <eugeniu.popa@isa.utm.md>
2022-09-11 22:30:42 +0300

    add the tables

---
+++ components/tables.py
@@ -0,0 +1,70 @@
+global table_state1
+global table_state2
+global table_state3
+global tables
+
+table_state1 = 'The table is free'
+table_state2 = 'The table is waiting to make an order'
+table_state3 = 'The table is waiting for the order to be served'
+
+tables = [
+    {
+        "id": 1,
+        "state": table_state1,
+        "order_id": None
+    }, 
+    
+    {
+        "id": 2,
+        "state": table_state1,
+        "order_id": None
+    }, 
+    
+    {
+        "id": 3,
+        "state": table_state1,
+        "order_id": None
+    }, 
+    
+    {
+        "id": 4,
+        "state": table_state1,
+        "order_id": None
+    }, 
+    
+    {
+        "id": 5,
+        "state": table_state1,
+        "order_id": None 
+    },
+    
+    {
+        "id": 6,
+        "state": table_state1,
+        "order_id": None 
+    },
+    
+    {
+        "id": 7,
+        "state": table_state1,
+        "order_id": None 
+    },
+    
+    {
+        "id": 8,
+        "state": table_state1,
+        "order_id": None 
+    },
+    
+    {
+        "id": 9,
+        "state": table_state1,
+        "order_id": None 
+    },
+    
+    {
+        "id": 10,
+        "state": table_state1,
+        "order_id": None 
+    }
+]
