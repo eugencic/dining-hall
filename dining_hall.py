2824b1f940b4d4599f4dfbe9ce0d3cbcc390a335
Eugen <eugeniu.popa@isa.utm.md>
2022-09-12 00:14:09 +0300

    add the Clients thread

--- dining_hall.py
+++ dining_hall.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from threading import Thread
+import random
+import components.tables as tables
+import components.foods as menu
 
 # Create a Flask object called app
 app = Flask(__name__)
@@ -6,11 +10,13 @@ app = Flask(__name__)
 # App route
 @app.route('/distribution')
 
-def print_test():
-    print(f"Test run of the server")
-    
+# Customized Clients class extending the Thread class
+class Clients(Thread):
+    def __init__(self, *args, **kwargs):
+        # Access methods of the base class
+        super(Clients, self).__init__(*args, **kwargs)
+        
 def run_dinninghall():
-    print_test()
     app.run(host = '0.0.0.0', port = 3000, debug = True)
 
 if __name__ == '__main__':
