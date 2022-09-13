b4fe317ad7ce9221a586a8f676ebb8aaaa97db1e
Eugen <eugeniu.popa@isa.utm.md>
2022-09-13 16:18:54 +0300

    make the server run the threads properly

--- clientService/waiter.py
+++ clientService/waiter.py
@@ -28,10 +28,10 @@ class Waiter(Thread):
             # Take the table that made the order by table id 
             table_id = next((id for id, table in enumerate(tables) if table['id'] == order['table_id']), None)
             # Detailed message that the waiter took the order
-            print(f'Table nr.{order["table_id"]}. The waiter is taking the order nr.{order["id"]}. It has the priority {order["priority"]}, and foods: {order["items"]}.')
+            print(f'Table nr.{order["table_id"]}. The waiter is taking the order nr.{order["id"]}. It has the priority {order["priority"]}, and foods: {order["items"]}.\n')
             # Change the state of the table
             tables[table_id]['state'] = table_state2
-            time.sleep(3)
+            time.sleep(4)
             # Put the order data in a dictionary
             payload = dict({'table_id': order['table_id'], 'order_id': order['id'], 'items': order['items'], 'priority': order['priority']})
             requests.post('http://localhost:8000/order', json = payload, timeout = 0.0001)
--- dining_hall.py
+++ dining_hall.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from clientService.client import Client
 from clientService.waiter import Waiter
+from threading import Thread
 
 # Create a Flask object called app
 app = Flask(__name__)
@@ -13,11 +14,12 @@ threads = []
 
 def distribution():
     data = request.get_json()
-    print(f'Order nr.{data["order_id"]} is served to table nr.{data["table_id"]}.')
+    print(f'Order nr.{data["order_id"]} is served to table nr.{data["table_id"]}.\n')
     return {'success': True}
 
 def run_dinninghall():
-    app.run(host = '0.0.0.0', port = 3000, debug = False)
+    dining_hall_thread = Thread(target=lambda: app.run(host = '0.0.0.0', port = 3000, debug = False, use_reloader = False), daemon = True)
+    dining_hall_thread.start()
     # Create thread Client
     client_thread = Client()
     # Add the thread to the array
